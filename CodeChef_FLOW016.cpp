3780cf284cc04d6bb6f742082890627d904d3009
HridoyAlam <optimus5289@gmail.com>
2020-10-10 09:47:06 +0600

    GCD & LCM

--- CodeChef_FLOW016.cpp
+++ CodeChef_FLOW016.cpp
@@ -2,23 +2,23 @@
 #define MIN(num1, num2) num1 < num2 ? num1 : num2
 using namespace std;
 
-int gcd(int num1, int num2) {
+long long int gcd(long long int num1, long long int num2) {
     if( num2 == 0 ) {
         return num1;
     }
     return gcd(num2, num1 % num2);
 }
 
-int main(int argc, char *argv[]) {
-    int t;
-    scanf("%d", &t);
+int main() {
+    long long int t;
+    cin>>t;
 
     while( t-- ) {
-        int n, m, g, l;
-        scanf("%d%d", &n, &m);
+        long long int n, m, g, l;
+        cin>>n>>m;
         g = gcd(n, m);
         l = (n * m) / g;
-        printf("%d %d\n", g, l);
+        printf("%lld %lld\n", g, l);
     }
     return 0;
 }
