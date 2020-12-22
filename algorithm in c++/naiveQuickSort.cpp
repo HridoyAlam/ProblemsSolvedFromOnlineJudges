740e04422bc2823549f7f3e890bbd2db7f4c9246
HridoyAlam <optimus5289@gmail.com>
2020-12-23 01:05:00 +0600

    Naive QuickSort

---
+++ algorithm in c++/naiveQuickSort.cpp
@@ -0,0 +1,45 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int partitionArray(int a[], int p, int r){
+    int x = a[r];
+    int i = p-1;
+    for(int j=p; j<=r-1; j++){
+        if(a[j] <= x){
+            i +=1;
+            swap(a[i],a[j]);
+        }
+    }
+    swap(a[i+1], a[r]);
+return i+1;
+}
+
+void quicSort(int a[], int p, int r){
+    if(p < r){
+        int q = partitionArray(a,p,r);
+        quicSort(a,p,q-1);
+        quicSort(a,q+1,r);
+    }
+}
+
+void printArray(int arr[], int n){
+    for(int i=0; i < n; i++){
+        cout<<arr[i]<<" ";
+    }
+    cout<<endl;
+}
+
+int main(){
+    int arr[] = {1,3,4,3,1,7,9,3,2,4,70,32,70};
+    int n = sizeof(arr) / sizeof(arr[0]);
+
+    for(int i=0; i < n; i++){
+        cout<<arr[i]<<" ";
+    }
+    cout<<endl;
+    cout<<n<<endl;
+
+    quicSort(arr,0, n-1);
+    printArray(arr,n);
+return 0;
+}
