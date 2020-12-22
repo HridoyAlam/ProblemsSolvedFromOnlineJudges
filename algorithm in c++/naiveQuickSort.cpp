cf8b09db43fc4bcb3ea98bcf4d6f02506848c02d
HridoyAlam <optimus5289@gmail.com>
2020-12-23 01:16:46 +0600

    randomized add

--- algorithm in c++/naiveQuickSort.cpp
+++ algorithm in c++/naiveQuickSort.cpp
@@ -22,13 +22,27 @@ void quicSort(int a[], int p, int r){
     }
 }
 
+
+// Randomized Quick Sort
+int rand_partition(int a[], int p, int r){
+    srand(time(NULL));
+    int random = p+rand() % (r-p);
+    swap(a[random], a[r]);
+    return partitionArray(a,p,r);
+}
+void QuickSort_randomized(int a[], int p, int r){
+    if(p<r){
+        int q = rand_partition(a,p,r);
+        QuickSort_randomized(a,p,q-1);
+        QuickSort_randomized(a,q+1,r);
+    }
+}
 void printArray(int arr[], int n){
     for(int i=0; i < n; i++){
         cout<<arr[i]<<" ";
     }
     cout<<endl;
 }
-
 int main(){
     int arr[] = {1,3,4,3,1,7,9,3,2,4,70,32,70};
     int n = sizeof(arr) / sizeof(arr[0]);
@@ -39,7 +53,7 @@ int main(){
     cout<<endl;
     cout<<n<<endl;
 
-    quicSort(arr,0, n-1);
+    QuickSort_randomized(arr,0, n-1);
     printArray(arr,n);
 return 0;
 }
